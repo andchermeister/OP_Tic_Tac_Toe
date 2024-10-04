ee27f1794abadcc63958b5b352f3be4ecb95e5e6
Andrii Chernikov <99753757+andchermeister@users.noreply.github.com>
2024-10-04 10:20:30 +0100

    Added html and css, changed the logic to interact throught the DOM, would be nice to add some UI andmake a focus on factories

--- script.js
+++ script.js
@@ -1,123 +1,102 @@
+// function for handling players
+
 function Player(name, marker) {
     return {name, marker};
-};
+}
 
-function gameBoard () {
-    let board = Array(9).fill(null);
+// function for playing the game
 
-    function getBoard() {
-        return board;
+function play() {
+    let P1 = Player('First player', 'X');
+    let P2 = Player('Seconds player', 'O');
+    let currentPlayer = P1;
+    const cells = document.querySelectorAll('.cell');
+
+    document.getElementById('currentPlayer').innerHTML = `${currentPlayer.marker}'s turn`;
+
+    placeMarker();
+
+    // function for placing markers
+
+    function placeMarker(currentMarker) {
+
+        cells.forEach(cell => {
+            cell.addEventListener('click', function() {
+                if (cell.innerHTML === '') {
+                    cell.innerHTML = currentPlayer.marker;
+                    if (!checkWinner() && !checkDraw()) {
+                        switchPlayer();
+                    }
+                }
+            }, {once: true});
+        }
+        );
     }
 
-    function placeMarker(index, marker) {
-        if (index >= 0 && index < 9 && board[index] === null) {
-            board[index] = marker;
-            return true;
-        }
-        return false;
-    };
-
-    function resetBoard() {
-        board = Array(9).fill(null);
-    };
-
-    function printBoard() {
-        console.clear();
-        console.log(`${board[0]} | ${board[1]} | ${board[2]}`)
-        console.log('-----+------+-----')
-        console.log(`${board[3]} | ${board[4]} | ${board[5]}`)
-        console.log('-----+------+-----')
-        console.log(`${board[6]} | ${board[7]} | ${board[8]}`)
-    };
-
-    return {getBoard, placeMarker, resetBoard, printBoard}
-};
-
-function game(player1, player2) {
-    const P1 = Player(player1, 'X');
-    const P2 = Player(player2, 'O');
-    const GameBoard = gameBoard();
-
-    let currentPlayer = P1;
-    console.log(currentPlayer);
-    switchPlayer(P1, P2);
-    console.log(currentPlayer);
+    // function for switching player
 
     function switchPlayer() {
-        currentPlayer = currentPlayer === P1 ? P2 : P1;
+        currentPlayer = (currentPlayer === P1) ? P2 : P1;
+        document.getElementById('currentPlayer').innerHTML = `${currentPlayer.marker}'s turn`;
     };
 
-    function playRound(index) {
-        if (GameBoard.placeMarker(index, currentPlayer.marker)) {
-            GameBoard.printBoard();
-            if (checkWin()) {
-                console.log(`${currentPlayer.name} wins!`)
-                return true;
-            } else if (GameBoard.getBoard().every(cell => cell !== null)) {
-                console.log("It's a draw!")
-                return true;
-            }
-            switchPlayer();
-        } else {
-            console.log("Invalid move, try again.")
-        }
-        return false;
-    };
+    // function for checking winner
 
-    function checkWin() {
-        const Board = GameBoard.getBoard();
-        const WinningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
+    function checkWinner(board) {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal combs
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical combs
+            [0, 4, 8], [2, 4, 6] // diagonal combs
         ];
 
-        for (let combination of WinningCombinations) {
+        for (let combination of winningCombinations) {
             const [a, b, c] = combination;
 
-            if(Board[a] && Board[a] === Board[b] && Board[a] === Board[c]) {
+            if (
+                cells[a].innerHTML !== '' &&
+                cells[a].innerHTML === cells[b].innerHTML &&
+                cells[a].innerHTML === cells[c].innerHTML
+            ) {
+                alert(`${currentPlayer.marker} wins!`);
+                document.getElementById('currentPlayer').innerHTML = `${currentPlayer.marker} wins!`;
                 return true;
-            };
+            }
+            
         };
 
         return false;
-        
     };
 
+    // function for checking draw
 
-    function start() {
-        GameBoard.printBoard();
-        let gameOver = false;
-        const readline = require('readline').createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-
-
-        function askMove() {
-            readline.question(`${currentPlayer.name} (${currentPlayer.marker}), enter your move(0-8): `, (input) => {
-                const index = parseInt(input);
-                if (!isNaN(index)) {
-                    gameOver = playRound(index);
-                } else {
-                    console.log("Please enter a valid number between 0 and 8.")
-                }
-                if (!gameOver) {
-                    askMove();
-                } else {
-                    readline.close();
-                }
-            });
+    function checkDraw() {
+        for (let cell of cells) {
+            if(cell.innerHTML === '') {
+                return false;
+            }
         };
+        alert("It's a draw");
+        document.getElementById('currentPlayer').innerHTML = `It's a draw!`;
+        return true;
+    }
 
-        askMove();
+    // function for restarting the game
 
-    };
+   function restart() {
+    currentPlayer = P1;
 
-    return { start };
+    cells.forEach(cell => {
+        cell.innerHTML = ''; 
+    });
+
+    document.getElementById('currentPlayer').innerHTML = `${currentPlayer.marker}'s turn`;
+
+    placeMarker();
+
+   }
+   
+   document.getElementById('resetButton').onclick = restart;
 
 };
 
-const ticTacToeGame = game("Andrii", "Yuliia");
-ticTacToeGame.start();
+ 
